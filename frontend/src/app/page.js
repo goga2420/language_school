65fa0847076ab3bcd04c6270516c6124b9ed1856
ilya-shlom <ilyashlom32@gmail.com>
2025-04-15 21:51:28 +0300

    style: adaptive flex & h1 fonts

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -15,12 +15,12 @@ export default function Home() {
       <header className="flex justify-between">
         <div className="flex justify-left items-center">
           <Image className="items-left " src="/Itish.jpg" width={70} height={70} alt="Лого"/>
-          <h2 className="text-xl text-left text-black ">Айтишкино</h2>
+          <h1 className="text-xl text-left text-black ">Айтишкино</h1>
         </div>
         <div className="flex justify-left items-center">
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className="inline-flex items-center text-black justify-center mr-4 w-60 px-4 py-2 bg-white border border-gray-300 rounded-lg shadow-sm hover:bg-gray-50 focus:outline-none"
+            className="inline-flex items-center text-black justify-center mr-4 w-60 px-4 py-2 bg-white border border-gray-300 rounded-lg shadow-sm hover:bg-gray-50 focus:outline-none cursor-pointer"
           >
 
             <div className="flex items-center text-xs text-black">
@@ -52,15 +52,15 @@ export default function Home() {
         )}
         </div>
       </header>
-      <div className="bg-white flex-col text-center md:flex-row justify-center items-center gap-[50px] p-20 ">
-          <Image src="/robot_ambient.svg" width={300} height={300} alt="robot" />
+      <div className="flex bg-white max-md:flex-col flex-row text-center justify-center items-center gap-[50px] p-20 ">
+          <Image src="/robot_amb.svg" width={300} height={300} alt="robot" />
           <div className="flex flex-col gap-10 m-10">
             <div className="font-bold">
               <h2 >Запишитесь на пробное занятие в IT-Школе</h2>
             </div>
             <div className="flex flex-col gap-2">
-             <button className="bg-[#e6f0fc] px-4 py-2 text-[#2A63A4] text-3xs border border-[#2A63A4] shadow-lg shadow-[#2A63A4] rounded-lg shadow-sm transition delay-150 duration-300 ease-in-out hover:bg-[#2A63A4] hover:text-white"> НАЧАТЬ </button>
-             <button className="bg-[#ffeee3] px-4 py-2 text-[#d66e29] text-3xs border border-[#d66e29] shadow-lg shadow-[#d66e29] rounded-lg shadow-sm transition delay-150 duration-300 ease-in-out hover:bg-[#d66e29] hover:text-white"> У МЕНЯ УЖЕ ЕСТЬ АККАУНТ </button>
+             <button className="bg-[#e6f0fc] px-4 py-2 text-[#2A63A4] text-3xs border border-[#2A63A4] shadow-lg shadow-[#2A63A4] rounded-lg shadow-sm transition duration-500 ease-in-out hover:bg-[#2A63A4] hover:text-white cursor-pointer"> НАЧАТЬ </button>
+             <button className="bg-[#ffeee3] px-4 py-2 text-[#d66e29] text-3xs border border-[#d66e29] shadow-lg shadow-[#d66e29] rounded-lg shadow-sm transition duration-500 ease-in-out hover:bg-[#d66e29] hover:text-white cursor-pointer"> У МЕНЯ УЖЕ ЕСТЬ АККАУНТ </button>
              </div>
           </div>
       </div>
