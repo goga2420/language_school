3f3f7046bae315aeb98ee8dcb0d9e347d35e925e
goga <zgr@valcom.ru>
2025-04-15 21:26:09 +0300

    buttons upgrade

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
       <header className="flex justify-between">
         <div className="flex justify-left items-center">
           <Image className="items-left " src="/Itish.jpg" width={70} height={70} alt="Лого"/>
-          <p className="text-xl text-left text-black ">Айтишкино</p>
+          <h2 className="text-xl text-left text-black ">Айтишкино</h2>
         </div>
         <div className="flex justify-left items-center">
           <button
@@ -52,15 +52,15 @@ export default function Home() {
         )}
         </div>
       </header>
-      <div className="bg-white flex justify-center items-center gap-50 p-20">
-          <Image src="/robot.svg" width={300} height={300} alt="robot" />
+      <div className="bg-white flex-col text-center md:flex-row justify-center items-center gap-[50px] p-20 ">
+          <Image src="/robot_ambient.svg" width={300} height={300} alt="robot" />
           <div className="flex flex-col gap-10 m-10">
             <div className="font-bold">
-              <h2>Запишитесь на пробное занятие в IT-Школе</h2>
+              <h2 >Запишитесь на пробное занятие в IT-Школе</h2>
             </div>
             <div className="flex flex-col gap-2">
-             <button className="bg-[#2A63A4] px-4 py-2 text-white text-3xs border border-gray-300 rounded-lg shadow-sm"> НАЧАТЬ </button>
-             <button className="bg-white px-4 py-2 text-[#EC5F3B] text-3xs border border-gray-300 rounded-lg shadow-sm"> У МЕНЯ УЖЕ ЕСТЬ АККАУНТ </button>
+             <button className="bg-[#e6f0fc] px-4 py-2 text-[#2A63A4] text-3xs border border-[#2A63A4] shadow-lg shadow-[#2A63A4] rounded-lg shadow-sm transition delay-150 duration-300 ease-in-out hover:bg-[#2A63A4] hover:text-white"> НАЧАТЬ </button>
+             <button className="bg-[#ffeee3] px-4 py-2 text-[#d66e29] text-3xs border border-[#d66e29] shadow-lg shadow-[#d66e29] rounded-lg shadow-sm transition delay-150 duration-300 ease-in-out hover:bg-[#d66e29] hover:text-white"> У МЕНЯ УЖЕ ЕСТЬ АККАУНТ </button>
              </div>
           </div>
       </div>
