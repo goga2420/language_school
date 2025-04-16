2e09570e7fcc2ef4c583d2dca05871941069eff3
ilya-shlom <ilyashlom32@gmail.com>
2025-04-17 01:54:18 +0300

    small updates

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -9,6 +9,7 @@ export default function Home() {
   const [selectedCurrent, setSelectedCurrent] = useState("ЯЗЫК САЙТА");
   const [isOpen, setIsOpen] = useState(false);
   const [svgLink, setSvgLink] = useState('/robot.svg');
+  const [fadeIn, setFadeIn] = useState(false);
 
   const [isWebKit, setIsWebKit] = useState(false);
 
@@ -16,6 +17,10 @@ export default function Home() {
     const ua = navigator.userAgent;
     const isWebKitBrowser = /AppleWebKit/.test(ua) && !/Chrome/.test(ua);
     setIsWebKit(isWebKitBrowser);
+  }, []);
+
+  useEffect(() => {
+    setFadeIn(true);
   }, []);
 
   useEffect(() => {
@@ -35,10 +40,10 @@ export default function Home() {
   }, [isWebKit]);
 
   const languages = ["РУССКИЙ", "ENGLISH", "ESPAÑOL"];
-  const current_lang = ["ЯЗЫК САЙТА: ", "WEBSITE LANGUAGE: ", "IDIOMA DEL SITIO: "];
+  const current_lang = ["ЯЗЫК САЙТА: ", "LANGUAGE: ", "IDIOMA DEL SITIO: "];
   return (
     <div className="bg-white">
-      <header className="flex justify-between">
+      <header className="fixed flex justify-between w-full bg-white p-3 max-md:p-0 backdrop-blur-md">
         <div className="flex justify-left items-center">
           <Image className="items-left " src="/Itish.jpg" width={70} height={70} alt="Лого"/>
           <h1 className="text-xl text-left text-black ">Айтишкино</h1>
@@ -51,7 +56,7 @@ export default function Home() {
           >
 
             <div className="flex items-center text-xs text-black">
-              <span className="max-md:hidden mr-4">{selectedCurrent}</span>
+              <span className="max-md:hidden mr-4 font-md">{selectedCurrent}</span>
               <Globe className="w-4 h-4 mr-2 hidden max-md:block" />
               <span>{selected}</span>
               
@@ -80,15 +85,15 @@ export default function Home() {
         )}
         </div>
       </header>
-      <div className="flex bg-white max-md:flex-col flex-row text-center justify-center items-center gap-[50px] p-20 ">
+      <div className="flex bg-white max-md:flex-col flex-row text-center justify-center items-center gap-[100px] max-md:gap-10 p-5 pt-20 ">
           <Image src={svgLink} width={300} height={300} alt="robot" />
-          <div className="flex flex-col gap-10 m-10">
+          <div className={`flex flex-col gap-3 items-center transition-opacity duration-1000 ${fadeIn ? 'opacity-100' : 'opacity-0'}`}>
             <div className="font-bold">
               <h2 >Запишитесь на пробное занятие в IT-Школе</h2>
             </div>
             <div className="flex flex-col gap-3 items-center">
-             <button className="w-96 bg-[#e6f0fc] px-4 py-2 text-[#2A63A4] text-3xs border border-[#2A63A4] shadow-lg shadow-[#2A63A4] rounded-lg shadow-sm transition duration-500 ease-in-out hover:bg-[#2A63A4] hover:text-white cursor-pointer"> НАЧАТЬ </button>
-             <button className="w-96 bg-[#ffeee3] px-4 py-2 text-[#d66e29] text-3xs border border-[#d66e29] shadow-lg shadow-[#d66e29] rounded-lg shadow-sm transition duration-500 ease-in-out hover:bg-[#d66e29] hover:text-white cursor-pointer"> У МЕНЯ УЖЕ ЕСТЬ АККАУНТ </button>
+             <button className="w-96 max-md:w-60 bg-[#e6f0fc] px-4 py-2 text-[#2A63A4] text-3xs border border-[#2A63A4] shadow-lg shadow-[#2A63A4] rounded-lg shadow-sm transition duration-500 ease-in-out hover:bg-[#2A63A4] hover:text-white cursor-pointer"> НАЧАТЬ </button>
+             <button className="w-96 max-md:w-60 bg-[#ffeee3] px-4 py-2 text-[#d66e29] text-3xs border border-[#d66e29] shadow-lg shadow-[#d66e29] rounded-lg shadow-sm transition duration-500 ease-in-out hover:bg-[#d66e29] hover:text-white cursor-pointer"> У МЕНЯ УЖЕ ЕСТЬ АККАУНТ </button>
              </div>
           </div>
       </div>
