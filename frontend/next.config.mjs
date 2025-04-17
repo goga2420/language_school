7f965447c13453d869219be2cdbb7631ed3eef00
ilya-shlom <ilyashlom32@gmail.com>
2025-04-17 23:11:52 +0300

    ops: deploy

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/language_school/' : '',
+  basePath: isProd ? '/language_school' : '',
+  output: 'export'
+};
 
 export default nextConfig;
